6dd47a5e2c6ec0ef977c303201761b774fc15a27
ming <88813537+euphratesriver0216@users.noreply.github.com>
2021-11-03 12:21:11 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <h1>리액트 노드 프로젝트 테스트용</h1>
+      <h1>리액트 노드 프로젝트 테스트용 해민이 수정중</h1>
     </div>
   );
 }
