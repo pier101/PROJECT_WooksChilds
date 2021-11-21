f17f2ffc1550a47db60a38895e012d7dec476b08
pier101 <85658044+pier101@users.noreply.github.com>
2021-11-22 03:36:29 +0900

    delete

--- src/page/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Board } from "./main";
-export { default as Write } from "./write";
-export { default as List } from "./list";
-export { default as View } from "./view";
-// export { default as Head } from "./header";
-
-
-
